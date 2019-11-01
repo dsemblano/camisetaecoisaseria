829dfff17029127be87e0f15d2c2a1bfd9f93506
Daniel Semblano <dsemblano@gmail.com>
2019-11-01 13:44:47 -0300

    removing // REMOVE WP EMOJI

--- site/web/app/themes/nos/app/filters.php
+++ site/web/app/themes/nos/app/filters.php
@@ -89,3 +89,10 @@ add_filter('comments_template', function ($comments_template) {
 
     return $comments_template;
 }, 100);
+
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('admin_print_styles', 'print_emoji_styles');
