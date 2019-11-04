453474a953c2b2a9f3a1f2c8c085e19f016187a8
Daniel Semblano <dsemblano@gmail.com>
2019-11-04 19:20:10 -0300

    Logo custom font

--- site/web/app/themes/nos/resources/views/partials/header.blade.php
+++ site/web/app/themes/nos/resources/views/partials/header.blade.php
@@ -1,8 +1,10 @@
 <header class="banner bg-black">
-  <h1 class="container mx-auto">
+  <h1 class="container">
     <a class="brand" href="{{ home_url('/') }}">
+      <span>NÓS</span>
+      <span>CAMISETA É COISA SÉRIA</span>
       {{-- {{ get_bloginfo('name', 'display') }} --}}
-      <img src="@asset('images/logo.png')" alt="Nós camisetas" />
+      {{-- <img src="@asset('images/logo.png')" alt="Nós camisetas" /> --}}
     </a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
--- site/web/app/themes/nos/resources/views/partials/main-menu.blade.php
+++ site/web/app/themes/nos/resources/views/partials/main-menu.blade.php
@@ -1,4 +1,4 @@
-<div class="my-10 px-1 w-full overflow-hidden sm:my-10 sm:px-1 sm:w-1/3 md:my-10 md:px-1 md:w-1/3 lg:my-10 lg:px-1 lg:w-1/3 xl:my-10 xl:px-1 xl:w-1/3">
+<div class="main-menu my-10 px-1 w-full overflow-hidden sm:main-menu my-10 sm:px-1 sm:w-1/3 md:main-menu my-10 md:px-1 md:w-1/3 lg:main-menu my-10 lg:px-1 lg:w-1/3 xl:main-menu my-10 xl:px-1 xl:w-1/3">
   <img class="border-dotted h-32 w-32 rounded-full border-4" src="@asset('images/home/print.jpeg')" alt="imagem impressão avulsa">
   <div class="px-6 py-4">
     <h2 class="font-bold text-xl mb-2">Impressão digital avulsa</h2>
@@ -8,17 +8,21 @@
   </div>
 </div>
 
-<div class="my-10 px-1 w-full overflow-hidden sm:my-10 sm:px-1 sm:w-1/3 md:my-10 md:px-1 md:w-1/3 lg:my-10 lg:px-1 lg:w-1/3 xl:my-10 xl:px-1 xl:w-1/3">
-  <img class="border-dotted h-32 w-32 rounded-full border-4" src="@asset('images/home/customise2.jpeg')" alt="imagem customizar camisa">
+<div class="main-menu my-10 px-1 w-full overflow-hidden sm:main-menu my-10 sm:px-1 sm:w-1/3 md:main-menu my-10 md:px-1 md:w-1/3 lg:main-menu my-10 lg:px-1 lg:w-1/3 xl:main-menu my-10 xl:px-1 xl:w-1/3">
+  <a href="/design-editor">
+    <img class="border-dotted h-32 w-32 rounded-full border-4" src="@asset('images/home/customise2.jpeg')" alt="imagem customizar camisa">
+  </a>
   <div class="px-6 py-4">
-    <h2 class="font-bold text-xl mb-2">Faça sua Nós</h2>
+    <h2 class="font-bold text-xl mb-2">
+      <a href="/design-editor">Faça sua Nós</a>
+    </h2>
     <p class="text-gray-700 text-base">
       <a href="/design-editor">Customise sua camisa</a>
     </p>
   </div>
 </div>
 
-<div class="my-10 px-1 w-full overflow-hidden sm:my-10 sm:px-1 sm:w-1/3 md:my-10 md:px-1 md:w-1/3 lg:my-10 lg:px-1 lg:w-1/3 xl:my-10 xl:px-1 xl:w-1/3">
+<div class="main-menu my-10 px-1 w-full overflow-hidden sm:main-menu my-10 sm:px-1 sm:w-1/3 md:main-menu my-10 md:px-1 md:w-1/3 lg:main-menu my-10 lg:px-1 lg:w-1/3 xl:main-menu my-10 xl:px-1 xl:w-1/3">
   <img class="border-dotted h-32 w-32 rounded-full border-4" src="@asset('images/home/outsourcing.png')" alt="imagem outsourcing">
   <div class="px-6 py-4">
     <h2 class="font-bold text-xl mb-2">Outsourcing</h2>
