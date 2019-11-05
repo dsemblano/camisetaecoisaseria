617d39dcebf4eb858b90d86578b339ccfae83f32
Daniel Semblano <dsemblano@gmail.com>
2019-11-04 22:12:03 -0300

    Typography v1.1

--- site/web/app/themes/nos/resources/views/partials/last-products.blade.php
+++ site/web/app/themes/nos/resources/views/partials/last-products.blade.php
@@ -1,7 +1,7 @@
 @foreach ($destaques as $product)
   <li class="last-products sm: my-12 sm:px-1 sm:w-1/4 md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/4 xl:my-1 xl:px-1 xl:w-1/4">
     {!!$product->get_image()!!}
-    <h2>
+    <h2 class="text-2xl">
         <a href={{$product->get_permalink( )}}>{{$product->get_title()}}</a>
     </h2>
     {{$product->get_regular_price()}}
