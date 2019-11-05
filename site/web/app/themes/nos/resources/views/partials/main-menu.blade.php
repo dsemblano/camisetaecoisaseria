f138fbecc90a4e49d5c9e0ec1b2fe11e5b3b2828
Daniel Semblano <dsemblano@gmail.com>
2019-11-04 22:54:58 -0300

    removing custom font

--- site/web/app/themes/nos/resources/views/partials/main-menu.blade.php
+++ site/web/app/themes/nos/resources/views/partials/main-menu.blade.php
@@ -1,7 +1,7 @@
 <div class="main-menu my-10 px-1 w-full overflow-hidden sm:main-menu my-10 sm:px-1 sm:w-1/3 md:main-menu my-10 md:px-1 md:w-1/3 lg:main-menu my-10 lg:px-1 lg:w-1/3 xl:main-menu my-10 xl:px-1 xl:w-1/3">
   <img class="border-dotted h-32 w-32 rounded-full border-4" src="@asset('images/home/print.jpeg')" alt="imagem impressão avulsa">
   <div class="px-6 py-4">
-    <h2 class="font-bold text-3xl mb-2">Impressão digital avulsa</h2>
+    <h2 class="font-bold text-xl mb-2">Impressão digital avulsa</h2>
     <p class="text-gray-700 text-base">
       textos e imagens (sem compra)
     </p>
@@ -13,7 +13,7 @@
     <img class="border-dotted h-32 w-32 rounded-full border-4" src="@asset('images/home/customise2.jpeg')" alt="imagem customizar camisa">
   </a>
   <div class="px-6 py-4">
-    <h2 class="font-bold text-3xl mb-2">
+    <h2 class="font-bold text-xl mb-2">
       <a href="/design-editor">Faça sua Nós</a>
     </h2>
     <p class="text-gray-700 text-base">
@@ -25,7 +25,7 @@
 <div class="main-menu my-10 px-1 w-full overflow-hidden sm:main-menu my-10 sm:px-1 sm:w-1/3 md:main-menu my-10 md:px-1 md:w-1/3 lg:main-menu my-10 lg:px-1 lg:w-1/3 xl:main-menu my-10 xl:px-1 xl:w-1/3">
   <img class="border-dotted h-32 w-32 rounded-full border-4" src="@asset('images/home/outsourcing.png')" alt="imagem outsourcing">
   <div class="px-6 py-4">
-    <h2 class="font-bold text-3xl mb-2">Outsourcing</h2>
+    <h2 class="font-bold text-xl mb-2">Outsourcing</h2>
     <p class="text-gray-700 text-base">
       Terceirização
     </p>
