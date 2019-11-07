bb2e24c907db93f91f2139d5ae30f9f9c23d9a2d
Daniel Semblano <dsemblano@gmail.com>
2019-11-07 14:08:25 -0300

    layout typography Maven Pro v1.1

--- site/web/app/themes/nos/resources/views/partials/main-menu.blade.php
+++ site/web/app/themes/nos/resources/views/partials/main-menu.blade.php
@@ -1,8 +1,10 @@
 <div class="main-menu my-10 px-1 w-full overflow-hidden sm:main-menu my-10 sm:px-1 sm:w-1/3 md:main-menu my-10 md:px-1 md:w-1/3 lg:main-menu my-10 lg:px-1 lg:w-1/3 xl:main-menu my-10 xl:px-1 xl:w-1/3">
   <img class="border-dotted h-32 w-32 rounded-full border-4" src="@asset('images/home/print.jpeg')" alt="imagem impressão avulsa">
   <div class="px-6 py-4">
-    <h2 class="font-bold text-xl mb-2">Impressão digital avulsa</h2>
-    <p class="text-gray-700 text-base">
+    <h2 class="text-2xl mb-2">
+      <a href="/impressao-digital-avulsa">Impressão digital avulsa</a>
+    </h2>
+    <p class="text-gray-700 text-xl">
       textos e imagens (sem compra)
     </p>
   </div>
@@ -13,10 +15,10 @@
     <img class="border-dotted h-32 w-32 rounded-full border-4" src="@asset('images/home/customise2.jpeg')" alt="imagem customizar camisa">
   </a>
   <div class="px-6 py-4">
-    <h2 class="font-bold text-xl mb-2">
+    <h2 class="text-2xl mb-2">
       <a href="/design-editor">Faça sua Nós</a>
     </h2>
-    <p class="text-gray-700 text-base">
+    <p class="text-gray-700 text-xl">
       <a href="/design-editor">Customise sua camisa</a>
     </p>
   </div>
@@ -27,10 +29,10 @@
     <img class="border-dotted h-32 w-32 rounded-full border-4" src="@asset('images/home/outsourcing.png')" alt="imagem outsourcing">
   </a>
   <div class="px-6 py-4">
-    <h2 class="font-bold text-xl mb-2">
+    <h2 class="text-2xl mb-2">
       <a href="/outsourcing">Outsourcing</a>
     </h2>
-    <p class="text-gray-700 text-base">
+    <p class="text-gray-700 text-xl">
         <a href="/outsourcing">Terceirização</a>
     </p>
   </div>
