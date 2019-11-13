22ea221aced78b45c002955a4aea4a71180b6eb5
Daniel Semblano <dsemblano@gmail.com>
2019-11-13 00:34:52 -0300

    Logo change

--- site/web/app/themes/nos/resources/views/partials/header.blade.php
+++ site/web/app/themes/nos/resources/views/partials/header.blade.php
@@ -2,12 +2,12 @@
   <div class="container text-center flex flex-wrap">
     <div
       class="overflow-hidden overflow-hidden sm:px-1 sm:w-1/3 md:px-1 md:w-1/3 lg:px-1 lg:w-1/3 xl:px-1 xl:w-1/3">
-      <h1>
+      <h1 class="mt-2 mb-0">
         <a class="brand" href="{{ home_url('/') }}">
           {{-- <span>NÓS</span>
               <span>CAMISETA É COISA SÉRIA</span> --}}
           {{-- {{ get_bloginfo('name', 'display') }} --}}
-          <img src="@asset('images/logo.png')" alt="Nós camisetas" />
+          <img class="w-2/5" src="@asset('images/brand2.png')" alt="Nós camisetas" />
         </a>
       </h1>
     </div>
