0dab6539e96bbd738aa4982f9bbe3ee94bda5384
Daniel Semblano <dsemblano@gmail.com>
2019-11-21 18:48:22 -0300

    Novo layout v1.3.1

--- site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
@@ -1,6 +1,10 @@
-<section class="class">
-<h2 class="">FAÇA SUA NÓS</h2>
-<p class="">crie e customize todos os produtos</p>
-<button class="btn-estudio"><a href="/design-editor/">ESTÚDIO DE CRIAÇÃO</a></button>
-
+<section class="blockhome-3">
+  <div class="blockhome-3-left">
+      <h2 class="">FAÇA SUA NÓS</h2>
+      <p class="">crie e customize todos os produtos</p>
+      <button class="btn-estudio"><a href="/design-editor/">ESTÚDIO DE CRIAÇÃO</a></button>
+  </div>
+  <figure class="blockhome3-right">
+    <img src="@asset('images/home/blusabranca.png')" alt="blusa branca" class="c">
+  </figure>
 </section>
