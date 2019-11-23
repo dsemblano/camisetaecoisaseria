9cb6cebce12e13d54c77711a7fb2ab8fdb87ed90
Daniel Semblano <dsemblano@gmail.com>
2019-11-23 14:57:55 -0300

    Novo layout v1.6.1.2 instagram

--- site/web/app/themes/nos/resources/views/template-home.blade.php
+++ site/web/app/themes/nos/resources/views/template-home.blade.php
@@ -12,6 +12,7 @@
     @include('partials.home-block3')
     @include('partials.home-block4')
     @include('partials.home-slideshow')
+    {!! do_shortcode('[insta-gallery id="1"]') !!}
     {{-- <div class="text-center flex flex-wrap mt-10">
       @include('partials.main-menu')
     </div>
