6f71722abbb55dd077f596c5036862b024fe75c4
Daniel Semblano <dsemblano@gmail.com>
2019-11-25 06:10:34 -0300

    Novo layout v1.6.5.1 home fixes

--- site/web/app/themes/nos/resources/views/partials/header.blade.php
+++ site/web/app/themes/nos/resources/views/partials/header.blade.php
@@ -1,11 +1,12 @@
 <header class="banner bg-pureblack">
   <div class="container text-center">
-      <h1 class="pt-5 mb-0">
+      <h1 class="pt-5 mb-0 flex items-center justify-between">
         <a class="brand" href="{{ home_url('/') }}">
           {{-- <span>NÓS</span>
               <span>CAMISETA É COISA SÉRIA</span> --}}
           {{-- {{ get_bloginfo('name', 'display') }} --}}
           <img class="text-center" src="@asset('images/imprimanos.png')" alt="Nós camisetas" />
+          @php echo do_shortcode('[TheChamp-Sharing]') @endphp
         </a>
       </h1>
     </div>
