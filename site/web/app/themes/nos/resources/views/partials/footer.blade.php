a77f449cbea4a423ffe6b03844983dc60c03cbf0
Daniel Semblano <dsemblano@gmail.com>
2019-11-25 05:45:15 -0300

    Novo layout v1.6.5 home fixes

--- site/web/app/themes/nos/resources/views/partials/footer.blade.php
+++ site/web/app/themes/nos/resources/views/partials/footer.blade.php
@@ -12,16 +12,17 @@
     </nav>
       @php dynamic_sidebar('sidebar-footerup') @endphp
   </div>
+
   <div class="footer-down bg-pureblack w-full py-6">
-    <div class="container">
-      <img src="@asset('images/footer.png')" alt="image footer" class="footer-img">
+    <div class="container flex items-center justify-between">
+      <img src="@asset('images/footer.png')" alt="image footer" class="footer-img w-1/4">
       <nav class="nav-secondary bg-pureblack">
         @if (has_nav_menu('secondary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'secondary_navigation', 'menu_class' => 'nav flex flex-row flex-wrap
-        content-between justify-between container text-white py-5 text-2xl uppercase']) !!}
+        {!! wp_nav_menu(['theme_location' => 'secondary_navigation', 'menu_class' => 'nav flex text-white py-5 text-2xl uppercase']) !!}
         @endif
       </nav>
-      @php dynamic_sidebar('sidebar-footer') @endphp
+      @php echo do_shortcode('[TheChamp-Sharing]') @endphp
     </div>
+    @php dynamic_sidebar('sidebar-footer') @endphp
   </div>
 </footer>
