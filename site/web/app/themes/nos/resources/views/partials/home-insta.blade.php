daf14987e02a35c38678754c9f00e1a09db0824a
Daniel Semblano <dsemblano@gmail.com>
2019-11-24 22:23:08 -0300

    Novo layout v1.6.4.1 home fixes

--- site/web/app/themes/nos/resources/views/partials/home-insta.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-insta.blade.php
@@ -1,4 +1,5 @@
-<div class="flex flex-col container">
-  <h2 class="title-insta text-right text-pureblack text-3xl">Siga nosso <br><img src="@asset('images/instalogo.png')" alt="instagram logo" class="img-insta w-56"></h2>
+<div class="h-40 flex flex-col relative">
+  <hr>
+  <h2 class="title-insta text-right text-pureblack text-3xl container absolute right-0">Siga nosso <br><img src="@asset('images/instalogo.png')" alt="instagram logo" class="img-insta w-56"></h2>
 </div>
 {!! do_shortcode('[insta-gallery id="1"]') !!}
