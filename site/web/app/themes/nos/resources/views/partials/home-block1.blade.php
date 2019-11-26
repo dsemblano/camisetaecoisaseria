8bd3e5c517290294d4ca13745e4a27b102a808ef
Daniel Semblano <dsemblano@gmail.com>
2019-11-26 18:58:55 -0300

    Novo layout v1.7.4 home fixes responsiveness

--- site/web/app/themes/nos/resources/views/partials/home-block1.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-block1.blade.php
@@ -1,4 +1,4 @@
-<section class="blockhome-1 h-slide1-xl flex flex-col flex-no-wrap justify-between">
+<section class="blockhome-1 h-slide1-sm md:h-slide1-xl flex flex-col flex-no-wrap justify-between">
 
   <div class="blockhome-1-container container pt-16 pb-2">
     <div class="block1-left text-white text-2xl mb-2 tracking-widest leading-loose">
@@ -11,7 +11,7 @@
   </div>
 
   <div class="block1-down border-solid border-black border-t-1 tracking-widest w-full">
-    <div class="container flex flex-col md:flex-row justify-between py-5 text-1xl md:text-2xl">
+    <div class="container flex flex-col text-center md:text-right md:flex-row justify-between py-5 text-1xl md:text-2xl">
       <span class="text-white">Impressão digital em algodão</span>
       <span class="text-pureblack">Camiseta é coisa séria</span>
     </div>
