8fa5c76950309a442d1b4674224f2d294bd181d1
Daniel Semblano <dsemblano@gmail.com>
2019-11-28 13:53:51 -0300

    Novo layout v1.8.7 home fixes responsiveness

--- site/web/app/themes/nos/resources/views/partials/home-block1.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-block1.blade.php
@@ -1,12 +1,13 @@
 <section class="blockhome-1 h-slide1-sm md:h-slide1-xl flex flex-col flex-no-wrap justify-between">
 
-  <div class="blockhome-1-container container pt-16 pb-2">
-    <div class="block1-left text-white text-2xl mb-2 tracking-widest leading-loose">
+  <div class="blockhome-1-container container pr-12 md:w-full pt-16 pb-2">
+    <div class="block1-left flex flex-col items-end md:block text-white text-2xl mb-2 tracking-widest leading-loose">
       <p>Imagine</p>
       <p>Crie</p>
       <p>Invente</p>
       <p>Imprima</p>
     </div>
+    <br>
     <button class="btn-estudio"><a href="/design-editor/">Estúdio de criação</a></button>
   </div>
 
