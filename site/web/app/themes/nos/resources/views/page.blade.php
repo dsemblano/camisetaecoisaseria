b2826492a92820a80c98c5e4075c30de9c2cb32d
Daniel Semblano <dsemblano@gmail.com>
2019-11-28 19:52:29 -0300

    page template styling

--- site/web/app/themes/nos/resources/views/page.blade.php
+++ site/web/app/themes/nos/resources/views/page.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="content container">
   @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
     @include('partials.content-page')
   @endwhile
+</div>
 @endsection
--- site/web/app/themes/nos/resources/views/partials/content.blade.php
+++ site/web/app/themes/nos/resources/views/partials/content.blade.php
@@ -1,3 +1,5 @@
+sadasas
+
 <article @php post_class() @endphp>
   <header>
     <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
