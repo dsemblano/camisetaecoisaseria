135d3b2c7a22f9a9b5e622e31dab6956462f9980
Daniel Semblano <dsemblano@gmail.com>
2019-11-29 01:39:49 -0300

    Novo layout v1.9 home fixes responsiveness

--- site/web/app/themes/nos/resources/views/partials/home-insta.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-insta.blade.php
@@ -1,22 +1,24 @@
-<div class="insta border-solid border-black pt-8 border-t-2 mb-12 h-56 md:h-40 flex flex-col relative">
-  <form class="email-input md:w-1/3 absolute">
-    <h3>Receba nossas promoções</h3>
-    {{-- {!! do_shortcode('[wd_hustle id="Newsletter" type="embedded"/]') !!} --}}
-    <div class="flex border-pureblack border-solid border-8">
-      <input class="w-full border-none text-grey-dark mr-3 py-1 pl-4 tracking-widest text-xl focus:outline-none"
-        type="text" placeholder="seuemail@exemplo.com.br" aria-label="Email">
-      <button class="text-xl text-white bg-pureblack p-4" type="button">
-        OK
-      </button>
+<section class="instagram border-solid border-black border-t-2 ">
+  <div class="insta container pt-8 mb-12 h-56 md:h-40 flex flex-col relative">
+    <form class="email-input md:w-1/3 absolute">
+      <h3>Receba nossas promoções</h3>
+      {{-- {!! do_shortcode('[wd_hustle id="Newsletter" type="embedded"/]') !!} --}}
+      <div class="flex border-pureblack border-solid border-8">
+        <input class="w-full border-none text-grey-dark mr-3 py-1 pl-4 tracking-widest text-xl focus:outline-none"
+          type="text" placeholder="seuemail@exemplo.com.br" aria-label="Email">
+        <button class="text-xl text-white bg-pureblack p-4" type="button">
+          OK
+        </button>
+      </div>
+    </form>
+    <div class="sigainsta absolute">
+      <hr class="insta-hr">
+      <h2 class="title-insta text-right text-pureblack md:w-1/4 text-3xl container absolute">
+        <img src="@asset('images/instalogo.png')" alt="instagram logo" class="img-insta">
+      </h2>
     </div>
-  </form>
-  <div class="sigainsta absolute">
-    <hr class="insta-hr">
-    <h2 class="title-insta text-right text-pureblack md:w-1/4 text-3xl container absolute">
-      <img src="@asset('images/instalogo.png')" alt="instagram logo" class="img-insta">
-    </h2>
   </div>
-</div>
 
-@include('partials.slideinsta')
+  @include('partials.slideinsta')
 
+</section>
--- site/web/app/themes/nos/resources/views/partials/slideshow.blade.php
+++ site/web/app/themes/nos/resources/views/partials/slideshow.blade.php
@@ -1,1 +1,4 @@
-{!! do_shortcode('[smartslider3 slider=4]') !!}
+<section class="slideshow mb-12">
+    {!! do_shortcode('[smartslider3 slider=4]') !!}
+</section>
+
