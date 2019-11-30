d242ceb751420d89edf6eaf6a813b9f6237be4fb
Daniel Semblano <dsemblano@gmail.com>
2019-11-30 00:09:36 -0300

    removing some extra improvments snippets v2

--- site/web/app/themes/nos/app/extra.php
+++ site/web/app/themes/nos/app/extra.php
@@ -1,79 +1,79 @@
 <?php
 
 //Remove JQuery migrate
-function remove_jquery_migrate($scripts)
-{
-    if (!is_admin() && isset($scripts->registered['jquery'])) {
-        $script = $scripts->registered['jquery'];
+// function remove_jquery_migrate($scripts)
+// {
+//     if (!is_admin() && isset($scripts->registered['jquery'])) {
+//         $script = $scripts->registered['jquery'];
 
-        if ($script->deps) { // Check whether the script has any dependencies
-            $script->deps = array_diff($script->deps, array(
-                'jquery-migrate'
-            ));
-        }
-    }
-}
+//         if ($script->deps) { // Check whether the script has any dependencies
+//             $script->deps = array_diff($script->deps, array(
+//                 'jquery-migrate'
+//             ));
+//         }
+//     }
+// }
 
-add_action('wp_default_scripts', 'remove_jquery_migrate');
+// add_action('wp_default_scripts', 'remove_jquery_migrate');
 
 
 // Woocommerce edits
 /**
  * Rename product data tabs
  */
-add_filter('woocommerce_product_tabs', 'woo_rename_tabs', 98);
-function woo_rename_tabs($tabs)
-{
-    $tabs['description']['title'] = __('Mais informações'); // Rename the description tab
-    $tabs['reviews']['title'] = __('Ratings'); // Rename the reviews tab
-    $tabs['additional_information']['title'] = __('Informações do produto'); // Rename the additional information tab
+// add_filter('woocommerce_product_tabs', 'woo_rename_tabs', 98);
+// function woo_rename_tabs($tabs)
+// {
+//     $tabs['description']['title'] = __('Mais informações'); // Rename the description tab
+//     $tabs['reviews']['title'] = __('Ratings'); // Rename the reviews tab
+//     $tabs['additional_information']['title'] = __('Informações do produto'); // Rename the additional information tab
 
-    return $tabs;
-}
+//     return $tabs;
+// }
 
 /**
  * Change number of related products output
  */
-function woo_related_products_limit()
-{
-    global $product;
+// function woo_related_products_limit()
+// {
+//     global $product;
 
-    $args['posts_per_page'] = 6;
-    return $args;
-}
+//     $args['posts_per_page'] = 6;
+//     return $args;
+// }
 
-add_filter('woocommerce_output_related_products_args', 'jk_related_products_args', 20);
-function jk_related_products_args($args)
-{
-    $args['posts_per_page'] = 8; // 4 related products
-    $args['columns'] = 4; // arranged in 2 columns
-    return $args;
-}
+// add_filter('woocommerce_output_related_products_args', 'jk_related_products_args', 20);
+// function jk_related_products_args($args)
+// {
+//     $args['posts_per_page'] = 8; // 4 related products
+//     $args['columns'] = 4; // arranged in 2 columns
+//     return $args;
+// }
 
 // Change WooCommerce "Related products" text
 
-add_filter('gettext', 'change_rp_text', 10, 3);
-add_filter('ngettext', 'change_rp_text', 10, 3);
+// add_filter('gettext', 'change_rp_text', 10, 3);
+// add_filter('ngettext', 'change_rp_text', 10, 3);
 
-function change_rp_text($translated, $text, $domain)
-{
-    if ($text === 'Related products' && $domain === 'woocommerce') {
-        $translated = esc_html__('Veja mais', $domain);
-    }
-    return $translated;
-}
+// function change_rp_text($translated, $text, $domain)
+// {
+//     if ($text === 'Related products' && $domain === 'woocommerce') {
+//         $translated = esc_html__('Veja mais', $domain);
+//     }
+//     return $translated;
+// }
 
-add_filter('woocommerce_product_add_to_cart_text', function ($text) {
-    if ('Read more' == $text) {
-        $text = esc_html__('Ver detalhes', 'woocommerce');
-    }
+// add_filter('woocommerce_product_add_to_cart_text', function ($text) {
+//     if ('Read more' == $text) {
+//         $text = esc_html__('Ver detalhes', 'woocommerce');
+//     }
 
-    if ('Add to cart' == $text) {
-        $text = esc_html__('Adicionar', 'woocommerce');
-    }
+//     if ('Add to cart' == $text) {
+//         $text = esc_html__('Adicionar', 'woocommerce');
+//     }
 
-    return $text;
-});
+//     return $text;
+// });
 
 /**
 * Change View cart text
@@ -171,12 +171,12 @@ function wps_deregister_styles()
 }
 add_action('wp_print_styles', 'wps_deregister_styles', 100);
 
-// Disable wp-embed.js
-function my_deregister_scripts()
-{
-    wp_deregister_script('wp-embed');
-}
-add_action('wp_footer', 'my_deregister_scripts');
+// // Disable wp-embed.js
+// function my_deregister_scripts()
+// {
+//     wp_deregister_script('wp-embed');
+// }
+// add_action('wp_footer', 'my_deregister_scripts');
 
 //removing WP version
 remove_action('wp_head', 'wp_generator');
