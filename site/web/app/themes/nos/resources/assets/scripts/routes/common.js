62bd472c456d9883453f790ef89bdda56eeb713c
Daniel Semblano <dsemblano@gmail.com>
2019-12-05 05:37:33 -0300

    sandwich menu

--- site/web/app/themes/nos/resources/assets/scripts/routes/common.js
+++ site/web/app/themes/nos/resources/assets/scripts/routes/common.js
@@ -1,6 +1,10 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    $('.hamburger').click(function() {
+      $(this).toggleClass('is-active');
+      $('#navigation').toggleClass('is-active');
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
