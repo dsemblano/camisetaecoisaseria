62d35436e911c6be72cd1b2b13721387f83b2035
Daniel Semblano <dsemblano@gmail.com>
2019-12-10 12:09:41 -0300

    Menu fixes mobile v2.2

--- site/web/app/themes/nos/resources/views/partials/header.blade.php
+++ site/web/app/themes/nos/resources/views/partials/header.blade.php
@@ -1,11 +1,11 @@
 <header class="banner bg-pureblack py-4">
-  <div class="container flex flex-row">
-    @include('partials.home-block2')
-    <h1 class="mb-0">
+  <div class="container flex flex-row justify-between">
+    <h1 class="mb-0 w-2/4">
       <a class="brand" href="{{ home_url('/') }}">
         <img src="@asset('images/logov3.png')" alt="NÓS camisetas" />
       </a>
     </h1>
+    @include('partials.home-block2')
     {{-- @include('partials/components.socialshare') --}}
   </div>
 </header>
