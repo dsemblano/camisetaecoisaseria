2a7567ace6abffbb195ff231746524be6602ec0e
Daniel Semblano <dsemblano@gmail.com>
2019-12-10 22:09:17 -0300

    Menu fixes mobile v2.4.1

--- site/web/app/themes/nos/resources/views/partials/header.blade.php
+++ site/web/app/themes/nos/resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header class="banner bg-pureblack py-4">
-  <div class="container flex flex-row justify-between">
+  <div class="container flex flex-row justify-between items-center">
     <h1 class="mb-0 w-2/4 md:w-1/5">
       <a class="brand" href="{{ home_url('/') }}">
         <img src="@asset('images/logov3.png')" alt="NÓS camisetas" />
