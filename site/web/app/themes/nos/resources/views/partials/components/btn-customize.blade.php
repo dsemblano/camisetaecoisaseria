573d44a625535a44c4571b207eb1b288fbd8ffa0
Daniel Semblano <dsemblano@gmail.com>
2019-12-10 23:05:26 -0300

    Menu fixes mobile v2.4.3 button cria

--- site/web/app/themes/nos/resources/views/partials/components/btn-customize.blade.php
+++ site/web/app/themes/nos/resources/views/partials/components/btn-customize.blade.php
@@ -1,3 +1,3 @@
 <a href="/design-editor/">
-  <img src="@asset('images/home/btn-cria.png')" alt="botao criar camisa" class="btn-cria w-15">
+  <img src="@asset('images/home/btn-cria.png')" alt="botao criar camisa" class="btn-cria w-3/5 md:w-15">
 </a>
