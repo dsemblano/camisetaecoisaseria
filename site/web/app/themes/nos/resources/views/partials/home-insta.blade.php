9b764fb9e3a6e0f0dfaa3d2e162cf92a156c3dcb
Daniel Semblano <dsemblano@gmail.com>
2019-12-14 02:22:13 -0300

    fixes v2.8.9 instagram title

--- site/web/app/themes/nos/resources/views/partials/home-insta.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-insta.blade.php
@@ -1,6 +1,6 @@
 <section class="instagram relative border-solid border-gray border-b-1 pb-4">
-  <div class="sigainsta absolute">
-    <hr class="insta-hr">
+  <div class="sigainsta relative">
+    <hr class="insta-hr absolute">
     <h2 class="title-insta text-right text-pureblack md:w-1/4 text-3xl container absolute">
       <img src="@asset('images/s1.svg')" alt="instagram logo" class="img-insta">
     </h2>
