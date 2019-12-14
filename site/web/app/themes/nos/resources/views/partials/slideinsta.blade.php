99b0a5f60aa77aade569b5929e337bc89d3721c7
Daniel Semblano <dsemblano@gmail.com>
2019-12-14 06:38:03 -0300

    fixes v2.9 instagram title

--- site/web/app/themes/nos/resources/views/partials/slideinsta.blade.php
+++ site/web/app/themes/nos/resources/views/partials/slideinsta.blade.php
@@ -1,4 +1,4 @@
-<section class="instagram-container container mt-56 sm:mt-20">
+<section class="instagram-container container sm:mt-20">
   {!! do_shortcode('[insta-gallery id="1"]') !!}
 </section>
 
