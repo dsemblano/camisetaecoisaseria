b44100f47bcab9781f73f3c5201f0e61c1923ad3
Daniel Semblano <dsemblano@gmail.com>
2019-12-14 21:49:38 -0300

    fixes pages

--- site/web/app/themes/nos/resources/views/page.blade.php
+++ site/web/app/themes/nos/resources/views/page.blade.php
@@ -4,7 +4,6 @@
 @if (!( is_page( 'cart' ) || is_cart() || is_page('checkout') || is_checkout()))
 
   {{-- @include('partials.home-block1') --}}
-  @include('partials.home-block2')
   <div class="content container">
     @while(have_posts()) @php the_post() @endphp
       @include('partials.page-header')
@@ -15,7 +14,6 @@
   @include('partials.slideinsta')
 
 @else
-  @include('partials.home-block2')
   <div class="content container">
     @while(have_posts()) @php the_post() @endphp
       @include('partials.page-header')
