60e4b1c7886f1412580e68cc271001f9eed3f9e6
Daniel Semblano <dsemblano@gmail.com>
2019-12-16 00:24:06 -0300

    fixes v2.9.5

--- site/web/app/themes/nos/resources/views/partials/home-block1.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-block1.blade.php
@@ -6,14 +6,16 @@
   </div>
 
   <div class="block1-down border-solid border-white border-t-1 tracking-widest w-full">
-    <div class="container flex flex-col text-center md:text-right md:flex-row justify-between text-1xl md:text-2xl">
-      <span class="text-white">Impressão digital em algodão</span>
-      <span class="text-white md:text-pureblack">Camiseta é coisa séria</span>
+    <div class="container flex flex-col text-center md:text-right md:flex-row justify-between text-1xl md:text-2xl text-white">
+      <span>SEM limite de cores</span>
+      <span>SEM pedido mínimo</span>
+      <span>100% algodão certificado</span>
+      <span>Produtos à pronta entrega</span>
     </div>
   </div>
 
 </section>
 
 <section class="tracking-widest py-5 text-1xl text-base md:text-1xl">
-  <p class="container text-center">SEM limite de cores | SEM pedido mínimo | Produtos à pronta entrega</p>
+  <p class="container text-center">IMAGINE | CRIE | INVENTE | IMPRIMA</p>
 </section>
