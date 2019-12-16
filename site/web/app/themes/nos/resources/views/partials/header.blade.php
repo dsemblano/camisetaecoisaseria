b3de0b6cd0e8ef4493df7cb68177c9474ad34cec
Daniel Semblano <dsemblano@gmail.com>
2019-12-16 06:08:29 -0300

    fixes v3 logo

--- site/web/app/themes/nos/resources/views/partials/header.blade.php
+++ site/web/app/themes/nos/resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="banner bg-pureblack py-4">
   <div class="container flex flex-row justify-between items-center">
-    <h1 class="mb-0 w-2/4 md:w-1/5">
+    <h1 class="mb-0 w-1/25 md:w-15">
       @include('partials/components.logo')
     </h1>
     @include('partials.home-block2')
