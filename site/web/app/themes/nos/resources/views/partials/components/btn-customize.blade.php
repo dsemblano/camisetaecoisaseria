6c155b3173d262e7e31f62f5adf6b759f1ac2d34
Daniel Semblano <dsemblano@gmail.com>
2019-12-18 09:15:27 -0300

    main square button is back

--- site/web/app/themes/nos/resources/views/partials/components/btn-customize.blade.php
+++ site/web/app/themes/nos/resources/views/partials/components/btn-customize.blade.php
@@ -1,7 +1,8 @@
 <div class="imprima-button text-center">
-  <h2 class="text-white">IMPRIMA SUA</h2>
+  {{-- <h2 class="text-white">IMPRIMA SUA</h2>
     <a href="/design-editor/">
       <img src="@asset('images/home/btn-cria.png')" alt="botao criar camisa" class="btn-cria">
     </a>
-  <h2 class="text-white">CAMISETA</h2>
+  <h2 class="text-white">CAMISETA</h2> --}}
+  <button class="btn-estudio">CRIE A SUA AQUI</button>
 </div>
