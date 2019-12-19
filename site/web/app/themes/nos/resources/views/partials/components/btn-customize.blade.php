1975160515e5b9c78b925921d429b795a6929940
Daniel Semblano <dsemblano@gmail.com>
2019-12-19 07:55:59 -0300

    layout fixes v5.3.1 homeblock3

--- site/web/app/themes/nos/resources/views/partials/components/btn-customize.blade.php
+++ site/web/app/themes/nos/resources/views/partials/components/btn-customize.blade.php
@@ -1,8 +1,3 @@
 <div class="imprima-button text-center">
-  {{-- <h2 class="text-white">IMPRIMA SUA</h2>
-    <a href="/design-editor/">
-      <img src="@asset('images/home/btn-cria.png')" alt="botao criar camisa" class="btn-cria">
-    </a>
-  <h2 class="text-white">CAMISETA</h2> --}}
   <button class="btn-estudio">CRIE A SUA AQUI</button>
 </div>
--- site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
@@ -1,8 +1,12 @@
 <section class="blockhome-3 container flex flex-col md:flex-row justify-between mt-0 md:mt-12 mb-12">
-  <div class="blockhome-3-left mt-8 md:mt-0 mb-8 self-center order-1 m-auto pt-0 md:pt-16 pb-2 text-center leading-loose">
-      <h2 class="text-4xl text-pureblack">CRIE E CUSTOMIZE</h2>
+  <div class="blockhome-3-left m-auto mt-8 md:mt-0 mb-8 self-center order-1 text-center leading-none">
+      <h2 class="text-4xl text-pureblack">Crie e Customize</h2>
       <p class="text-2xl mb-6">todos os produtos</p>
-      @include('partials/components.btn-customize')
+      <figure class="blockhome3-right order-2 items-end">
+        <div class="imprima-button text-center">
+          <button class="btn-estudio">CRIE OUTROS PRODUTOS</button>
+        </div>
+      </figure>
   </div>
   <figure class="blockhome3-right order-2 items-end">
     <img src="@asset('images/home/produtos.png')" alt="imagens produtos">
