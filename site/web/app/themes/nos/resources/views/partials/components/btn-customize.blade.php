58522ea99df34c0e96da671c7117c12b91e6eaf3
Daniel Semblano <dsemblano@gmail.com>
2019-12-20 14:17:07 -0300

    layout fixes v5.1.1 homeblock1

--- site/web/app/themes/nos/resources/views/partials/components/btn-customize.blade.php
+++ site/web/app/themes/nos/resources/views/partials/components/btn-customize.blade.php
@@ -1,5 +1,7 @@
 <div class="imprima-button text-center">
   <h2 class="imprima-title">IMPRIMA SUA</h2>
+  <a href="/design-editor">
     <button class="btn-estudio">CLIQUE AQUI</button>
+  </a>
   <h2 class="imprima-title">CAMISETA</h2>
 </div>
