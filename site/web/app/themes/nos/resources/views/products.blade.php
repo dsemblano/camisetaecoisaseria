9965f25637c6ccb1ae5fadbebb9605e1d09f94e2
Daniel Semblano <dsemblano@gmail.com>
2019-12-22 09:13:43 -0300

    products page v1.4.1

--- site/web/app/themes/nos/resources/views/products.blade.php
+++ site/web/app/themes/nos/resources/views/products.blade.php
@@ -10,7 +10,7 @@
   @while(have_posts()) @php the_post() @endphp
     <ul class="flex flex-wrap p-0 overflow-hidden xl:-mx-1">
       @foreach ($destaques_produtos as $product)
-        <li class="last-products my-2 px-2 w-1/2 overflow-hidden sm:w-1/4 xl:my-1 xl:px-5 xl:w-1/4">
+        <li class="last-products my-2 px-2 w-1/2 overflow-hidden sm:w-1/4 xl:mt-1 xl:mb-8 xl:px-5 xl:w-1/4">
           {!!$product->get_image()!!}
           <h2 class="text-base mt-3">
               <a href={{$product->get_permalink( )}}>{{$product->get_title()}}</a>
