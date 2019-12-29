ef8a805e496b77a965096761d0b50b68f86976f8
Daniel Semblano <dsemblano@gmail.com>
2019-12-29 13:29:39 -0300

    home products block3 a href links v.1.4

--- site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
@@ -2,7 +2,7 @@
   <div class="blockhome-3-left m-auto mt-8 md:mt-0 mb-8 my-1 px-1 w-1/2 overflow-hidden text-center leading-none">
       <h2 class="text-4xl text-pureblack">Crie e Customize</h2>
       <p class="text-2xl mb-6">todos os produtos</p>
-      <figure class="blockhome3-right order-2 items-end">
+      <figure class="order-2 items-end">
         <div class="imprima-button text-center">
           <a href="/design-editor">
             <button class="btn-estudio">CRIE OUTROS PRODUTOS</button>
@@ -11,7 +11,7 @@
       </figure>
   </div>
   <div class="my-1 px-1 w-1/2 overflow-hidden">
-    <figure class="blockhome3-right flex flex-wrap -mx-1 overflow-hidden">
+    <div class="blockhome3-right flex flex-wrap -mx-1 overflow-hidden">
       @foreach ($sub_categories as $sub)
         @php
           $destaques_produtos = wc_get_products(array(
@@ -23,14 +23,16 @@
         @endphp
         @foreach ($destaques_produtos as $img)
           {{-- @php $img_prod = wp_get_attachment_url($img->get_image_id(), $size = 'homeblock3' ) @endphp --}}
-          <a href="{!! $img->get_permalink() !!}">
-            {!! $img->get_image($size = 'homeblock3') !!}
-          </a>
+          <figure>
+            <a href="{!! $img->get_permalink() !!}">
+              {!! $img->get_image($size = 'homeblock3') !!}
+            </a>
+          </figure>
           {{-- <img src="{{$img_prod}}" class="my-1 px-1 w-1/2 overflow-hidden attachment-homeblock3" alt="imagens produtos"> --}}
 
         @endforeach
       @endforeach
       {{-- <img src="@asset('images/home/produtos.png')" alt="imagens produtos"> --}}
-    </figure>
+        </div>
   </div>
 </section>
