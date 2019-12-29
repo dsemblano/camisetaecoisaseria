789135012a49b7d03a9f1b7e37a5a8fe31294c0c
Daniel Semblano <dsemblano@gmail.com>
2019-12-29 18:43:01 -0300

    home products block3 v.1.6.1

--- site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
@@ -1,7 +1,7 @@
 <section class="blockhome-3 container flex flex-wrap items-center -mx-1 overflow-hidden mt-0 md:mt-12 mb-12">
-  <div class="blockhome-3-left m-auto mt-8 md:mt-0 mb-8 my-1 px-1 w-1/2 overflow-hidden text-center leading-none">
-      <h2 class="text-4xl text-pureblack">Crie e Customize</h2>
-      <p class="text-2xl mb-6">todos os produtos</p>
+  <div class="blockhome-3-left text-md m-auto mt-8 md:mt-0 mb-8 my-1 px-1 w-1/2 overflow-hidden text-center leading-none">
+      <h2 class="md:text-4xl text-pureblack">Crie e Customize</h2>
+      <p class="md:text-2xl mb-6">todos os produtos</p>
       <figure class="order-2 items-end">
         <div class="imprima-button text-center">
           <a href="/design-editor">
