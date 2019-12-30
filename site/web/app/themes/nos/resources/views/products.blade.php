ba97399958d0d0ced1ff423270f8b667e06b549d
Daniel Semblano <dsemblano@gmail.com>
2019-12-30 13:54:54 -0300

    products page link a href v.1.7.2

--- site/web/app/themes/nos/resources/views/products.blade.php
+++ site/web/app/themes/nos/resources/views/products.blade.php
@@ -23,22 +23,24 @@
           @endphp
             @foreach ($destaques_produtos as $product)
               <li class="my-2 px-2 w-1/2 overflow-hidden sm:w-1/6 md:w-1/6">
-                {!! $product->get_image() !!}
-                <h2 class="text-sm mt-3 h-8">
-                  <a href="{!! $product->get_permalink() !!}">{!! $product->get_title() !!}</a>
-                </h2>
+                <a href="{!! $product->get_permalink() !!}">
+                  {!! $product->get_image() !!}
+                  <h2 class="text-sm mt-3 h-8">
+                    {!! $product->get_title() !!}
+                  </h2>
 
-                @if ($product->get_price())
-                  @php
-                    $product_base = get_post_meta($product->get_id(), 'lumise_product_base', true);
-                  @endphp
-                  <span>R$ {!! $product->get_price() !!}</span>
-                  <div class="text-xs text-center w-40 mt-4 lumise-button-customize btn-estudio-products uppercase">
-                    <a href="/design-editor/?product_cms={!! $product->get_id() !!}&product_base={{ $product_base }}">
-                      Crie a sua aqui
-                    </a>
-                  </div>
-                @endif
+                  @if ($product->get_price())
+                    @php
+                      $product_base = get_post_meta($product->get_id(), 'lumise_product_base', true);
+                    @endphp
+                    <span>R$ {!! $product->get_price() !!}</span>
+                    <div class="text-xs text-center w-40 mt-4 lumise-button-customize btn-estudio-products uppercase">
+                      <a href="/design-editor/?product_cms={!! $product->get_id() !!}&product_base={{ $product_base }}">
+                        Crie a sua aqui
+                      </a>
+                    </div>
+                  @endif
+                  </a>
 
                   @php do_action( 'woocommerce_after_shop_loop_item' ); @endphp
               </li>
