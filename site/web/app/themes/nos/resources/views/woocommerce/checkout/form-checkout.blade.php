1eb79bcf0d451cef47e70385a79f67c10e041ab4
Daniel Semblano <dsemblano@gmail.com>
2020-01-10 17:25:15 -0300

    checkout page v.1.1

--- site/web/app/themes/nos/resources/views/woocommerce/checkout/form-checkout.blade.php
+++ site/web/app/themes/nos/resources/views/woocommerce/checkout/form-checkout.blade.php
@@ -38,30 +38,32 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
   <div class="flex container">
     <div class="col2-set" id="customer_details">
-      <div class="col-1">
+      {{-- <div class="col-1"> --}}
         <?php do_action( 'woocommerce_checkout_billing' ); ?>
-      </div>
+      {{-- </div> --}}
 
-      <div class="col-2">
+      {{-- <div class="col-2"> --}}
         <?php do_action( 'woocommerce_checkout_shipping' ); ?>
+      {{-- </div> --}}
+    </div>
+
+    <div class="pedido">
+      <?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
+
+      <?php endif; ?>
+
+      <?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
+
+      <h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
+
+      <?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+
+      <div id="order_review" class="woocommerce-checkout-review-order">
+        <?php do_action( 'woocommerce_checkout_order_review' ); ?>
       </div>
+
+      <?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
     </div>
-
-    <?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
-
-    <?php endif; ?>
-
-    <?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
-
-    <h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
-
-    <?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
-
-    <div id="order_review" class="woocommerce-checkout-review-order">
-      <?php do_action( 'woocommerce_checkout_order_review' ); ?>
-    </div>
-
-    <?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
 
 </form>
 
