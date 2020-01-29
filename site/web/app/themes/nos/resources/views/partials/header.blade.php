78bbf4e969447a0bc84444dabc726614ae9dfc16
Daniel Semblano <dsemblano@gmail.com>
2020-01-29 09:36:18 -0300

    new logo v1.3 padding

--- site/web/app/themes/nos/resources/views/partials/header.blade.php
+++ site/web/app/themes/nos/resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header class="banner bg-pureblack py-4">
+<header class="banner bg-pureblack pt-3 pb-1">
   <div class="container flex flex-row justify-between items-center">
     <h1 class="mb-0 w-1/25 md:w-15">
       @include('partials/components.logo')
