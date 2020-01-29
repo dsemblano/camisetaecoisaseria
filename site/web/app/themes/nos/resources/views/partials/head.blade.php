ca91ee7edf20b603a59585ed0db8d7c2776554be
Daniel Semblano <dsemblano@gmail.com>
2020-01-29 10:43:09 -0300

    Favicon v2

--- site/web/app/themes/nos/resources/views/partials/head.blade.php
+++ site/web/app/themes/nos/resources/views/partials/head.blade.php
@@ -2,19 +2,34 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="57x57" href="@asset('images/favicon/apple-icon-57x57.png')">
+  <link rel="apple-touch-icon" sizes="60x60" href="@asset('images/favicon/apple-icon-60x60.png')">
+  <link rel="apple-touch-icon" sizes="72x72" href="@asset('images/favicon/apple-icon-72x72.png')">
+  <link rel="apple-touch-icon" sizes="76x76" href="@asset('images/favicon/apple-icon-76x76.png')">
+  <link rel="apple-touch-icon" sizes="114x114" href="@asset('images/favicon/apple-icon-114x114.png')">
+  <link rel="apple-touch-icon" sizes="120x120" href="@asset('images/favicon/apple-icon-120x120.png')">
+  <link rel="apple-touch-icon" sizes="144x144" href="@asset('images/favicon/apple-icon-144x144.png')">
+  <link rel="apple-touch-icon" sizes="152x152" href="@asset('images/favicon/apple-icon-152x152.png')">
   <link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicon/apple-touch-icon.png')">
+  <link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab.svg')" color="#5bbad5">
+  <link rel="shortcut icon" href="@asset('images/favicon/favicon.ico')">
+  <link rel="icon" type="image/png" sizes="192x192" href="@asset('images/favicon/android-icon-192x192.png')">
+  <link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicon/favicon-32x32.png')">
+  <link rel="icon" type="image/png" sizes="96x96" href="@asset('images/favicon/favicon-96x96.png')">
+  <link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png')">
   <link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicon/favicon-32x32.png')">
   <link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png')">
   <link rel="manifest" href="@asset('images/favicon/site.webmanifest')">
-  <link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab.svg')" color="#5bbad5">
-  <link rel="shortcut icon" href="@asset('images/favicon/favicon.ico')">
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
   <meta name="apple-mobile-web-app-title" content="Imprima Sua Camiseta">
   <meta name="application-name" content="Imprima Sua Camiseta">
+  <link rel="manifest" href="@asset('images/favicon/manifest.json')">
   <meta name="msapplication-config" content="@asset('images/favicon/browserconfig.xml')">
   <meta name="msapplication-TileColor" content="#2b5797">
+  <meta name="msapplication-TileImage" content="@asset('images/favicon/ms-icon-144x144.png')">
   <meta name="theme-color" content="#ff0000">
+
 
   @php wp_head() @endphp
 </head>
