2d9a32957dcea7ffa892ab99f69c68ef7610785e
Daniel Semblano <dsemblano@gmail.com>
2020-01-29 10:24:20 -0300

    Favicon v1.1

--- site/web/app/themes/nos/resources/views/partials/head.blade.php
+++ site/web/app/themes/nos/resources/views/partials/head.blade.php
@@ -7,7 +7,14 @@
   <link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png')">
   <link rel="manifest" href="@asset('images/favicon/site.webmanifest')">
   <link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab.svg')" color="#5bbad5">
+  <link rel="shortcut icon" href="@asset('images/favicon/favicon.ico')">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="Imprima Sua Camiseta">
+  <meta name="application-name" content="Imprima Sua Camiseta">
+  <meta name="msapplication-config" content="@asset('images/favicon/browserconfig.xml')">
   <meta name="msapplication-TileColor" content="#2b5797">
   <meta name="theme-color" content="#ff0000">
+
   @php wp_head() @endphp
 </head>
