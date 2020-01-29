3bbbed3bc222a3a26bfc0771d5fd6a98948c0913
Daniel Semblano <dsemblano@gmail.com>
2020-01-29 09:18:52 -0300

    Servicos h2 home link to servicos page

--- site/web/app/themes/nos/resources/views/partials/home-block4.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-block4.blade.php
@@ -1,5 +1,9 @@
 <section class="blockhome-4 md:my-16">
-  <h2 class="text-4xl mb-8 pt-8 md:text-5xl text-center text-pureblack border-solid border-black border-t-2">Serviços</h2>
+  <h2 class="text-4xl mb-8 pt-8 md:text-5xl text-center text-pureblack border-solid border-black border-t-2">
+    <a href="/servicos">
+      Serviços
+    </a>
+  </h2>
   <ul class="inline-flex container flex-col md:flex-row items-center justify-between items-center text-center p-0 mb-0">
     <li class="w-full mb-8 md:w-1/25">
       <a href="/outsourcing-dropshipping/">
