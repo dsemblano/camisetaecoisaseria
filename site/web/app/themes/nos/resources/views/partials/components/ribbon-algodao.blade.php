725284463645a5eb0618aa03c7f329c269e08f5f
Daniel Semblano <dsemblano@gmail.com>
2020-01-31 07:35:57 -0300

    Changed ribbon text

--- site/web/app/themes/nos/resources/views/partials/components/ribbon-algodao.blade.php
+++ site/web/app/themes/nos/resources/views/partials/components/ribbon-algodao.blade.php
@@ -1,1 +1,1 @@
-<div class="mb-12 py-3 text-2xl text-white text-center bg-grey-nos">100% Algodão Orgânico Certificado</div>
+<div class="mb-12 py-3 text-2xl text-white text-center bg-grey-nos">FRETE GRÁTIS PRA TODO O BRASIL em compras a partir de R$ 200,00</div>
