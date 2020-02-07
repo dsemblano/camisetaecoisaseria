393983c5be7088322303407b778b674d5c4367aa
Daniel Semblano <dsemblano@gmail.com>
2020-02-07 09:31:31 -0300

    mailchimp code

---
+++ site/web/app/themes/nos/resources/views/partials/components/newsletter.blade.php
@@ -0,0 +1,12 @@
+<div class="newsletter mb-20 md:mb-1 mt-20 md:h-40 flex flex-col justify-center items-baseline md:flex-row relative">
+  <h3 class="mb-4 md:mr-12">UM EMAIL POR MÊS E NADA MAIS</h3>
+  <form class="email-input w-full md:w-2/5">
+    {{-- <h3>Receba nossas promoções</h3> --}}
+    {!! do_shortcode('[mc4wp_form id="4400"]') !!}
+    {{-- <div class="flex border-pureblack border-solid border-8">
+      <input class="w-full border-none text-grey-dark mr-3 py-1 pl-4 tracking-widest text-xl focus:outline-none"
+        type="email" name="EMAIL" value="Inscreva-se" placeholder="seuemail@exemplo.com.br" aria-label="Email">
+      <input type="submit" value="OK" class="text-xl text-white bg-pureblack p-4" />
+    </div> --}}
+  </form>
+</div>
--- site/web/app/themes/nos/resources/views/partials/slideshow.blade.php
+++ site/web/app/themes/nos/resources/views/partials/slideshow.blade.php
@@ -1,20 +1,7 @@
 <section class="slideshow border-b-1 border-solid border-black">
   <div class="container">
     {!! do_shortcode('[smartslider3 slider=4]') !!}
-    <div class="newsletter mb-20 md:mb-1 mt-20 md:h-40 flex flex-col justify-center items-baseline md:flex-row relative">
-      <h3 class="mb-4 md:mr-12">UM EMAIL POR MÊS E NADA MAIS</h3>
-      <form class="email-input w-full md:w-2/5">
-        {{-- <h3>Receba nossas promoções</h3> --}}
-        {{-- {!! do_shortcode('[wd_hustle id="Newsletter" type="embedded"/]') !!} --}}
-        <div class="flex border-pureblack border-solid border-8">
-          <input class="w-full border-none text-grey-dark mr-3 py-1 pl-4 tracking-widest text-xl focus:outline-none"
-            type="text" placeholder="seuemail@exemplo.com.br" aria-label="Email">
-          <button class="text-xl text-white bg-pureblack p-4" type="button">
-            OK
-          </button>
-        </div>
-      </form>
-    </div>
+    @include('partials/components.newsletter')
   </div>
 </section>
 
