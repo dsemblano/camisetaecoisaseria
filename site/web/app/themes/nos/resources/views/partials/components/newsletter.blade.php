dcfc09b5112e37de29a7b356c22ee5c306271a90
Daniel Semblano <dsemblano@gmail.com>
2020-02-10 11:56:06 -0300

    newsletter plugin code and css

--- site/web/app/themes/nos/resources/views/partials/components/newsletter.blade.php
+++ site/web/app/themes/nos/resources/views/partials/components/newsletter.blade.php
@@ -1,8 +1,20 @@
 <div class="newsletter mb-20 md:mb-1 mt-20 md:h-40 flex flex-col justify-center items-baseline md:flex-row relative">
   <h3 class="mb-4 md:mr-12">UM EMAIL POR MÊS E NADA MAIS</h3>
-  <form class="email-input w-full md:w-2/5">
+  <form class="email-input w-full md:w-1/3">
     {{-- <h3>Receba nossas promoções</h3> --}}
-    {!! do_shortcode('[mc4wp_form id="4400"]') !!}
+    <div class="tnp tnp-subscription flex flex-wrap border-pureblack border-solid border-8">
+      <form method="post" action="http://camisetaecoisaseria.test/?na=s" onsubmit="return newsletter_check(this)">
+        <input type="hidden" name="nlang" value="">
+        <div class="tnp-field tnp-field-email w-5/6 overflow-hidden border-none text-grey-dark tracking-widest text-xl focus:outline-none">
+          {{-- <label>Email</label> --}}
+          <input class="tnp-email" placeholder="seuemail@exemplo.com.br" type="email" name="ne" required>
+        </div>
+        <div class="tnp-field tnp-field-button w-1/6 overflow-hidden text-xl text-white bg-pureblack">
+          <input class="tnp-submit" type="submit" value="OK" >
+        </div>
+      </form>
+      </div>
+
     {{-- <div class="flex border-pureblack border-solid border-8">
       <input class="w-full border-none text-grey-dark mr-3 py-1 pl-4 tracking-widest text-xl focus:outline-none"
         type="email" name="EMAIL" value="Inscreva-se" placeholder="seuemail@exemplo.com.br" aria-label="Email">
