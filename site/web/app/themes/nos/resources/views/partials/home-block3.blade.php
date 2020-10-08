a01d09ff2477f1d226b93464f9a1e04b1bd9bfb1
Daniel Semblano <dsemblano@gmail.com>
2020-10-08 14:26:28 -0300

    home block3 3 columns fixes

--- site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
@@ -23,7 +23,7 @@
         @endphp --}}
         @foreach ($home_products as $img)
           {{-- @php $img_prod = wp_get_attachment_url($img->get_image_id(), $size = 'homeblock3' ) @endphp --}}
-              <a class="linkimgprod my-2 px-2 w-1/2 overflow-hidden md:w-1/3" href="{!! $img->get_permalink() !!}">
+              <a class="linkimgprod my-2 px-2 w-full overflow-hidden sm:w-1/3" href="{!! $img->get_permalink() !!}">
                 @php
                   $imgprod = $img->get_image($size = 'produtos', array('class'=>'imgprod w-full'));
                 @endphp
