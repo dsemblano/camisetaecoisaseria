823e3c0f8385f278b5466251fc5bc827c98c79ee
Daniel Semblano <dsemblano@gmail.com>
2020-11-17 15:06:06 -0300

    camisetacolab v1.1

--- site/web/app/themes/nos/resources/views/woocommerce/camisetacolab.blade.php
+++ site/web/app/themes/nos/resources/views/woocommerce/camisetacolab.blade.php
@@ -9,7 +9,7 @@
 
   <div class="content products container">
     <section class="category-products flex flex-col my-12 border-solid border-gray border-b-1 pb-6">
-      <ul class="nos_products flex flex-wrap -mx-4 p-0 text-center">
+      {{-- <ul class="nos_products flex flex-wrap -mx-4 p-0 text-center">
         @foreach ($bandejas_imp as $img)
         <li class="my-4 px-4 w-1/2 overflow-hidden sm:w-1/4 md:w-1/4">
           <a class="linkimgprod my-2 px-2 w-1/2 overflow-hidden sm:w-1/3" href="{!! $img->get_permalink() !!}">
@@ -25,7 +25,36 @@
         </li>
         @endforeach
 
-      </ul>
+      </ul> --}}
+      @php
+      // The product category taxonomy
+      $taxonomy = 'product_cat';
+
+      // Get the parent categories IDs
+      $parent_cat_ids = get_terms( $taxonomy, array(
+      'parent' => 0,
+      'hide_empty' => false,
+      'fields' => 'ids'
+      ) );
+
+      // Get only "child" WP_Term Product categories
+      $subcategories = get_terms( $taxonomy, array(
+      'exclude' => $parent_cat_ids,
+      'orderby' => 'name',
+      'order' => 'asc',
+      'hide_empty' => false,
+      ) );
+
+      if( ! empty( $subcategories ) ){
+      echo '<ul>';
+        foreach ($subcategories as $subcategory) {
+        echo '<li>
+          <a href="'. get_term_link($subcategory) .'">' . $subcategory->name.'</a>
+        </li>';
+        }
+        echo '</ul>';
+      }
+      @endphp
     </section>
   </div>
 
