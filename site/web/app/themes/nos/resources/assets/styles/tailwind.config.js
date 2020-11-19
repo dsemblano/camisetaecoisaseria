4987f6f993181eaa4c59979f263283f3c80c81a4
Daniel Semblano <dsemblano@gmail.com>
2020-11-19 19:20:35 -0300

    css fixes products v2

--- site/web/app/themes/nos/resources/assets/styles/tailwind.config.js
+++ site/web/app/themes/nos/resources/assets/styles/tailwind.config.js
@@ -59,6 +59,8 @@ let colors = {
   'grey-lighter': '#f1f5f8',
   'grey-lightest': '#f8fafc',
   'white': '#ffffff',
+  'h': '#2e2e2e',
+  'p': '#333333',
 
   'red-darkest': '#3b0d0c',
   'red-darker': '#621b18',
