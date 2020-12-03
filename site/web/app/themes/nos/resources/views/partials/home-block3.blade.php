da87fd40af6894934387b793a64172b08112f6ad
Daniel Semblano <dsemblano@gmail.com>
2020-12-03 16:38:13 -0300

    Home Block 3 changes v1.1

--- site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
@@ -1,5 +1,5 @@
 <section class="blockhome blockhome-3 container flex flex-col overflow-hidden mt-8">
-  <div class="blockhome-3-left px-2 w-full overflow-hidden md:w-1/2 text-md m-auto text-center leading-none">
+  <div class="blockhome-3-left mt-4 mb-8 px-2 w-full overflow-hidden md:w-1/2 text-md m-auto text-center leading-none">
       <h2 class="md:text-4xl text-pureblack">Crie e Customize</h2>
       <p class="md:text-2xl mb-6">todos os produtos</p>
       <figure class="order-2 items-end">
@@ -22,12 +22,12 @@
         @endphp --}}
         @foreach ($home_products as $img)
           {{-- @php $img_prod = wp_get_attachment_url($img->get_image_id(), $size = 'homeblock3' ) @endphp --}}
-              <a class="linkimgprod my-2 px-2 w-full overflow-hidden sm:w-1/3" href="{!! $img->get_permalink() !!}">
+              <a class="linkimgprod my-2 px-2 w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/3" href="{!! $img->get_permalink() !!}">
                 @php
                   $imgprod = $img->get_image($size = 'produtos', array('class'=>'imgprod w-full'));
                 @endphp
                 {!! $imgprod !!}
-                <h2 class="text-sm mt-3 h-12 text-center">
+                <h2 class="mt-3 h-12 text-center">
                   {!! $img->get_title() !!}
                 </h2>
               </a>
