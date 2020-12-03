9bed2059816115f617f095ac16f948bb63126360
Daniel Semblano <dsemblano@gmail.com>
2020-12-03 16:20:11 -0300

    Home Block 3 changes

--- site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-block3.blade.php
@@ -1,4 +1,4 @@
-<section class="blockhome blockhome-3 container flex flex-wrap overflow-hidden mt-8">
+<section class="blockhome blockhome-3 container flex flex-col overflow-hidden mt-8">
   <div class="blockhome-3-left px-2 w-full overflow-hidden md:w-1/2 text-md m-auto text-center leading-none">
       <h2 class="md:text-4xl text-pureblack">Crie e Customize</h2>
       <p class="md:text-2xl mb-6">todos os produtos</p>
@@ -10,7 +10,6 @@
         </div>
       </figure>
   </div>
-  <div class="my-2 px-2 w-full overflow-hidden md:w-1/2">
     <div class="blockhome3-right flex flex-wrap -mx-2 overflow-hidden">
       {{-- @foreach ($sub_categories as $sub)
         @php
@@ -23,7 +22,7 @@
         @endphp --}}
         @foreach ($home_products as $img)
           {{-- @php $img_prod = wp_get_attachment_url($img->get_image_id(), $size = 'homeblock3' ) @endphp --}}
-              <a class="linkimgprod my-2 px-2 w-1/2 overflow-hidden sm:w-1/3" href="{!! $img->get_permalink() !!}">
+              <a class="linkimgprod my-2 px-2 w-full overflow-hidden sm:w-1/3" href="{!! $img->get_permalink() !!}">
                 @php
                   $imgprod = $img->get_image($size = 'produtos', array('class'=>'imgprod w-full'));
                 @endphp
@@ -36,5 +35,4 @@
       {{-- @endforeach --}}
       {{-- <img src="@asset('images/home/produtos.png')" alt="imagens produtos"> --}}
       </div>
-  </div>
 </section>
