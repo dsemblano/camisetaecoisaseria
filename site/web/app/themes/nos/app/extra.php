814842073f1ee90fd0acfeb894eb2e0631fe0509
Daniel Semblano <dsemblano@gmail.com>
2020-12-07 15:17:46 -0300

    if (is_product_category('faca-sua-camiseta')) for buy button

--- site/web/app/themes/nos/app/extra.php
+++ site/web/app/themes/nos/app/extra.php
@@ -396,10 +396,14 @@ function yourtheme_setup()
 
 
     // To change add to cart text on single product page
-    add_filter( 'woocommerce_product_single_add_to_cart_text', 'woocommerce_custom_single_add_to_cart_text' );
-    function woocommerce_custom_single_add_to_cart_text() {
-        return __( 'Compre sem customizar', 'woocommerce' );
+    if (is_product_category('faca-sua-camiseta')) {
+        add_filter('woocommerce_product_single_add_to_cart_text', 'woocommerce_custom_single_add_to_cart_text');
+        function woocommerce_custom_single_add_to_cart_text()
+        {
+            return __('Compre sem customizar', 'woocommerce');
+        }
     }
+
 
     // To change add to cart text on product archives(Collection) page
     // add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_add_to_cart_text' );
