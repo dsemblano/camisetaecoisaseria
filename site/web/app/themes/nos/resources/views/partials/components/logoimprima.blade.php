421173d951d162f82f0618b35cc490b259166cd5
Daniel Semblano <dsemblano@gmail.com>
2020-12-28 14:09:21 -0300

    menu improvements v2.1

--- site/web/app/themes/nos/resources/views/partials/components/logoimprima.blade.php
+++ site/web/app/themes/nos/resources/views/partials/components/logoimprima.blade.php
@@ -1,3 +1,3 @@
-<a class="brand_imprima" href="/design-editor">
+<a class="w-1/3 overflow-hidden lg:w-auto brand_imprima" href="/design-editor">
   <img src="@asset('images/imprima_sua_ideia.png')" alt="Logo site" />
 </a>
--- site/web/app/themes/nos/resources/views/partials/header.blade.php
+++ site/web/app/themes/nos/resources/views/partials/header.blade.php
@@ -4,13 +4,13 @@
     <header class="banner relative bg-grey-darkest">
 @endif
   <div class="container max-w-4xl px-8 lg:px-0 flex flex-row justify-around lg:justify-between items-center">
-    <div class="md:flex md:flex-row md:w-full lg:hidden class1">
-      @include('partials.home-block2')
-    </div>
-    <h1 class="ml-12 xl:ml-0 logoimprima">
+    <h1 class="w-1/3 overflow-hidden lg:w-auto lg:ml-12 xl:ml-0 logoimprima">
       @include('partials/components.logo')
     </h1>
-    <div class="hidden lg:flex lg:flex-row lg:w-full items-center class2 relative">
+    <div class="w-1/3 overflow-hidden md:flex md:flex-row lg:hidden text-center class1">
+      @include('partials.home-block2')
+    </div>
+    <div class="w-1/3 overflow-hidden hidden lg:flex lg:flex-row lg:w-full items-center class2 relative">
       @include('partials.home-block2')
       {{-- <div class="whatsapp">
         {!! do_shortcode('[ht-ctc-chat]') !!}
