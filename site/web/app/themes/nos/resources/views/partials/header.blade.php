9939389f9eccb64876d4a65ededfff76e88365d9
Daniel Semblano <dsemblano@gmail.com>
2020-12-29 16:25:50 -0300

    menu improvements heigh mobile and desktop

--- site/web/app/themes/nos/resources/views/partials/header.blade.php
+++ site/web/app/themes/nos/resources/views/partials/header.blade.php
@@ -3,11 +3,11 @@
   @else
     <header class="banner relative bg-grey-darkest">
 @endif
-  <div class="container h-16 pt-4 max-w-4xl lg:px-0 flex flex-row justify-around lg:justify-between items-center">
+  <div class="container h-16 lg:h-32 pt-4 max-w-4xl lg:px-0 flex flex-row justify-around lg:justify-between items-center">
     <h1 class="w-1/4 overflow-hidden lg:w-auto lg:ml-12 xl:ml-0 logoimprima">
       @include('partials/components.logo')
     </h1>
-    <div class="w-2/4 overflow-hidden md:flex md:flex-row lg:hidden text-center class1">
+    <div class="w-2/4 overflow-hidden pb-4 md:flex md:flex-row lg:hidden text-center class1">
       @include('partials.home-block2')
     </div>
     <div class="w-1/4 overflow-hidden hidden lg:flex lg:flex-row lg:w-full mb-8 items-center menu-desktop relative">
