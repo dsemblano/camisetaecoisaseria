9cc29f91497b159a59bb7437c8708740a88d536a
Daniel Semblano <dsemblano@gmail.com>
2021-04-29 21:03:15 -0300

    ribbon text changed

--- site/web/app/themes/nos/resources/views/partials/components/ribbon-algodao.blade.php
+++ site/web/app/themes/nos/resources/views/partials/components/ribbon-algodao.blade.php
@@ -1,1 +1,1 @@
-<div class="mb-12 py-3 text-lg2 text-white text-center bg-grey-nos">Frete grátis nas compras acima de R$200</div>
+<div class="mb-12 py-3 text-lg2 text-white text-center bg-grey-nos">Frete grátis nas compras acima de R$250</div>
