ad2cd5dbd91fc83a24db214b4b9332b5f9400490
Daniel Semblano <dsemblano@gmail.com>
2021-05-17 12:25:52 -0300

    width and height img

--- site/web/app/themes/nos/resources/views/partials/components/logo.blade.php
+++ site/web/app/themes/nos/resources/views/partials/components/logo.blade.php
@@ -1,3 +1,3 @@
 <a class="brand relative" href="{{ home_url('/') }}">
-  <img src="@asset('images/logo_imprima.png')" alt="Logo site" />
+  <img src="@asset('images/logo_imprima.png')" alt="Logo site" width="200" height="107" />
 </a>
--- site/web/app/themes/nos/resources/views/partials/components/logoimprima.blade.php
+++ site/web/app/themes/nos/resources/views/partials/components/logoimprima.blade.php
@@ -1,3 +1,3 @@
 <a class="w-1/3 overflow-hidden lg:w-auto brand_imprima" href="/design-editor">
-  <img src="@asset('images/imprima_sua_ideia.png')" alt="Logo site" />
+  <img src="@asset('images/imprima_sua_ideia.png')" alt="Logo site" width="271" height="238" />
 </a>
