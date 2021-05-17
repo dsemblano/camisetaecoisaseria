e09832c8968f5c826a1bc0ee17304aa61448dd5c
Daniel Semblano <dsemblano@gmail.com>
2021-05-17 12:40:28 -0300

    width and height img  fixes

--- site/web/app/themes/nos/resources/views/partials/home-block1.blade.php
+++ site/web/app/themes/nos/resources/views/partials/home-block1.blade.php
@@ -4,21 +4,21 @@
       <div class="container text-sm md:ml-auto md:text-xl text-left text-white font-bold w-full">
         <div class="container-up">
           <div class="flex items-center py-2 md:py-0 leading-tight w-full">
-            <img src="@asset('images/home/block1/semlimites.png')" alt="ícone sem limite cores" />
+            <img src="@asset('images/home/block1/semlimites.png')" alt="ícone sem limite cores" width="118" height="45" />
             {{-- <span class="ml-4 text-xs md:text-lg text-pureblack"><p>SEM</p><p>limite de cores</p></span> --}}
           </div>
           <div class="flex items-center py-2 md:py-0 leading-tight w-full">
-            <img src="@asset('images/home/block1/sempedido.png')" alt="ícone sem pedido" />
+            <img src="@asset('images/home/block1/sempedido.png')" alt="ícone sem pedido" width="137" height="38" />
             {{-- <span class="ml-4 text-xs md:text-lg text-pureblack"><p>SEM</p><p>pedido mínimo</p></span> --}}
           </div>
         </div>
         <div class="container-up">
           <div class="flex items-center py-2 md:py-0 leading-tight w-full">
-            <img src="@asset('images/home/block1/100algodao.png')" alt=" ícone 100% algodão" />
+            <img src="@asset('images/home/block1/100algodao.png')" alt=" ícone 100% algodão" width="107" height="39" />
             {{-- <span class="ml-4 text-xs md:text-lg text-pureblack"><p>100% algodão</span> --}}
           </div>
           <div class="flex items-center py-2 md:py-0 leading-tight w-full">
-            <img src="@asset('images/home/block1/produtos_entrega.png')" alt="ícone produtos pronta entrega.png" />
+            <img src="@asset('images/home/block1/produtos_entrega.png')" alt="ícone produtos pronta entrega.png" width="189" height="31" />
             {{-- <span class="ml-4 text-xs md:text-lg text-pureblack"><p>Produtos à</p><p>pronta entrega</p></span> --}}
           </div>
         </div>
