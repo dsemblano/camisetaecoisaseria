25a452ff3a25f0332b2daa5a8d6e9d8000f5150e
Daniel Semblano <dsemblano@gmail.com>
2022-07-21 14:01:35 -0300

    cnpj chaged

--- site/web/app/themes/nos/resources/views/partials/components/copyright.blade.php
+++ site/web/app/themes/nos/resources/views/partials/components/copyright.blade.php
@@ -1,7 +1,6 @@
 <section class="widget text-2 widget_text">
   <p>
     <a href="https://api.whatsapp.com/send?phone=5541991181577" title="Mande sua mensagem via Whatsapp para NØS!"
-      target="_blank" class="inline-block whatslink" aria-label="Whatsapp mensagem">Whatsapp: (41) 99171-6676</a> - CNPJ:
-    36.409.056/0001-41 © Todos os direitos reservados. {{ date('Y') }}
+      target="_blank" class="inline-block whatslink" aria-label="Whatsapp mensagem">Whatsapp: (41) 99171-6676</a> - CNPJ: 19.312.401/0001-0541 © Todos os direitos reservados. {{ date('Y') }}
   </p>
 </section>
