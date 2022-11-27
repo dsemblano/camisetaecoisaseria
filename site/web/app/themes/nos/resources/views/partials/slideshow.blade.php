55f6657a925f8fc33649f7e60fe287c6f953819d
Daniel Semblano <dsemblano@gmail.com>
2022-11-27 18:44:36 -0300

    removed slideshow code sidebar dynamic

--- site/web/app/themes/nos/resources/views/partials/slideshow.blade.php
+++ site/web/app/themes/nos/resources/views/partials/slideshow.blade.php
@@ -1,5 +1,4 @@
-<section class="slideshow">
+{{-- <section class="slideshow">
   {!! do_shortcode('[smartslider3 slider=9]') !!}
-  {{-- @include('partials/components.newsletter') --}}
 </section>
-
+ --}}
