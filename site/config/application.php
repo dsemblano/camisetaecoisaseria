dfc95e4d7876e52a01f91c20f918153d15d7cc04
Daniel Semblano <dsemblano@gmail.com>
2022-12-06 12:20:35 -0300

    disabled debugging prod, css changes

--- site/config/application.php
+++ site/config/application.php
@@ -103,8 +103,8 @@ Config::define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?: true);
 /**
  * Debugging Settings
  */
-Config::define('WP_DEBUG_DISPLAY', true);
-Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
+Config::define('WP_DEBUG_DISPLAY', false);
+Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? false);
 Config::define('SCRIPT_DEBUG', false);
 ini_set('display_errors', '0');
 
